72bf7d49f11b4ac22669775387932726799a58b7
Future Tech Admin <artcreativity2020@gmail.com>
2025-12-17 15:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ import connectDB from "./config/database.js";
 
 const app = express();
 
-// DB connect
 await connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Backend running");
-});
+app.get("/", (req, res) => res.send("Backend running"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
-console.log("DB URL exists:", !!process.env.MYSQL_PUBLIC_URL);
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
